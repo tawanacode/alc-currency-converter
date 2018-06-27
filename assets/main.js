ce5972b7c65ed5c51b5d6ee810a673ba7d293ba1
tawanacode <info@tawanacode.com>
2018-06-27 23:18:06 +0200

    continue updating layout and JS file

--- assets/main.js
+++ assets/main.js
@@ -4,23 +4,23 @@ $(document).ready(function () {
   const convertTo = document.querySelector("#convertTo");
   const selectedConvertFrom = document.querySelector("#convertFrom option[selected]").innerHTML;
   const selectedConvertTo = document.querySelector("#convertTo option[selected]").innerHTML;
-
+  const convertBtn = document.querySelector("#convertBtn");
   //inputVal.addEventListener('keydown', function () {
-    $.getJSON("https://free.currencyconverterapi.com/api/v5/currencies", function (json) {
-      const currencies = json.results;
-      console.log(currencies);
-      for(let currency in currencies){
+    //$.getJSON("https://free.currencyconverterapi.com/api/v5/currencies", function (json) {
+      //const currencies = json.results;
+      //console.log(currencies);
+      //for(let currency in currencies){
 
-        console.log(currencies[currency].currencyName, currencies[currency].id); 
-      }
-    });
+        //console.log(currencies[currency].currencyName, currencies[currency].id); 
+      //}
+    //});
   //});
 
-  inputVal.addEventListener('keydown', function () {
-    $.getJSON("https://free.currencyconverterapi.com/api/v5/convert?q="+selectedConvertFrom+"_"+selectedConvertTo+"&callback=?", function (json) {
-      const exchangeRate = json.results[`${selectedConvertFrom}_${selectedConvertTo}`].val;
-      const results = Number.parseFloat(exchangeRate * inputVal.value).toFixed(3);
-        $("#convertedValue").html(results);
+  convertBtn.addEventListener("click", function () {
+   $.getJSON("https://free.currencyconverterapi.com/api/v5/convert?q="+selectedConvertFrom+"_"+selectedConvertTo+"&callback=?", function (json) {
+    const exchangeRate = json.results[`${selectedConvertFrom}_${selectedConvertTo}`].val;
+    const results = Number.parseFloat(exchangeRate * inputVal.value).toFixed(3);
+      $("#convertedValue").html(results);
     });
   });
 
