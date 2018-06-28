5006ed0cf262344456824f82d0c3ee2e58a491cd
tawanacode <info@tawanacode.com>
2018-06-28 17:18:09 +0200

    make all change events correspond to selected currencies

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,68 @@
-$(document).ready(function () {
-  const inputVal = document.querySelector("#inputValue");
-  const convertFrom = document.querySelector("#convertFrom");
-  const convertTo = document.querySelector("#convertTo");
-  const selectedConvertFrom = document.querySelector("#convertFrom option[selected]").innerHTML;
-  const selectedConvertTo = document.querySelector("#convertTo option[selected]").innerHTML;
-  const convertBtn = document.querySelector("#convertBtn");
-  //inputVal.addEventListener('keydown', function () {
-    //$.getJSON("https://free.currencyconverterapi.com/api/v5/currencies", function (json) {
-      //const currencies = json.results;
-      //console.log(currencies);
-      //for(let currency in currencies){
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
 
-        //console.log(currencies[currency].currencyName, currencies[currency].id); 
-      //}
-    //});
-  //});
+    const inputVal = document.querySelector("#inputValue");
+    let select = document.querySelectorAll("select");
+    const fromSymbol = document.querySelector("#convertFromSymbol");
+    const toSymbol = document.querySelector("#convertToSymbol");
+    const convertedVal = document.querySelector("#convertedValue");
 
-  convertBtn.addEventListener("click", function () {
-   $.getJSON("https://free.currencyconverterapi.com/api/v5/convert?q="+selectedConvertFrom+"_"+selectedConvertTo+"&callback=?", function (json) {
-    const exchangeRate = json.results[`${selectedConvertFrom}_${selectedConvertTo}`].val;
-    const results = Number.parseFloat(exchangeRate * inputVal.value).toFixed(3);
-      $("#convertedValue").html(results);
+    let convertFrom = document.querySelector("#convertFrom option[selected]");
+    let convertTo = document.querySelector("#convertTo option[selected]");
+
+    let selectedConvertFrom = convertFrom.value;
+    let selectedConvertTo = convertTo.value;
+    const convertBtn = document.querySelector("#convertBtn");
+
+    //console.log(select);
+    fetch("https://free.currencyconverterapi.com/api/v5/currencies").then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      //console.log(json.results);
+      //populate select dropdowns with currency name
+      Array.prototype.forEach.call(select, function (e) {
+        for (let prop in json.results) {
+          const option = document.createElement("option");
+          option.text = json.results[prop].currencyName;
+          option.value = json.results[prop].id;
+          e.appendChild(option);
+        }
+
+        e.addEventListener("change", function (el) {
+          //show the current country name in the select
+          const selectId = el.target.previousElementSibling.firstElementChild.id;
+          const inputId = selectId.endsWith("To") ? "convertSymbolTo" : "convertSymbolFrom";
+          
+          const oldCurrencyVal = document.querySelector(`#${selectId}`);
+          const oldCurrencySymbol = document.querySelector(`#${inputId}`);
+
+          const newCurrencyVal = el.target.selectedOptions[0].value;
+          const newCurrencySymbol = json.results[newCurrencyVal].currencySymbol;
+
+          //update selected currency
+          //console.log(oldCurrencySymbol);
+         // console.log(newCurrencyVal.currencySymbol);
+          oldCurrencyVal.innerHTML = newCurrencyVal;
+
+          //show currency unit next to country
+          oldCurrencySymbol.innerHTML = (newCurrencySymbol !== undefined) ? newCurrencySymbol : newCurrencyVal;
+
+          //convertBtn.click();
+        });
+      });
+    });
+
+    convertBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log(selectedConvertFrom);
+      fetch("https://free.currencyconverterapi.com/api/v5/convert?q=" + selectedConvertFrom + "_" + selectedConvertTo).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        const exchangeRate = json.results[`${selectedConvertFrom}_${selectedConvertTo}`].val;
+        const results = Number.parseFloat(exchangeRate * inputVal.value).toFixed(2);
+        convertedVal.innerHTML = results;
+      });
     });
   });
-
-});
+})();
