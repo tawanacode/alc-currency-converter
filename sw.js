1cba677dc5c237d8d2bfba7cd50dbc08c838f640
tawanacode <info@tawanacode.com>
2018-07-01 00:00:52 +0200

    add fetch code to service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'currency-converter-v1';
+var staticCacheName = 'v1';
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
@@ -32,6 +32,51 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
+    
+  e.respondWith(
+    caches.match(e.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+            
+        console.log("response");
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = e.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              //console.log(response);
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(staticCacheName)
+              .then(function(cache) {
+                
+              console.log("cloned");
+                cache.put(e.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+//self.addEventListener('fetch', function(e) {
  // var requestUrl = new URL(e.request.url);
 
   //if (requestUrl.origin === location.origin) {
@@ -46,4 +91,4 @@ self.addEventListener('fetch', function(e) {
     //  return response || fetch(e.request);
 //    })
 //  );
-});
+//});
